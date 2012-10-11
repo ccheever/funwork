d9b8c9d9e5f93af0cf68ae267ccee15ca137ae0c
Chalrie Cheever <ccheever@gmail.com>
2012-10-11 06:55:27 +0000

    replacing server.py with the old server2.py

--- server.py
+++ server.py
@@ -1,50 +1,36 @@
-import tornado.ioloop
-import tornado.web
-
 import getpass
-USER = getpass.getuser()
+import os
 
+from flask import Flask
+app = Flask(__name__)
+
+USER = getpass.getuser()
 if USER == "aiba":
     PORT = 8880
 elif USER == "ccheever":
     PORT = 8881
 else:
-    PORT = 8888
+    PORT = 5000
 
+def local_file(file_name):
+    return file(os.path.join(os.path.abspath(os.path.join(__file__, os.path.pardir)), file_name)).read()
 
+@app.route('/')
+def hello_world():
+    return 'Hello zoom!'
 
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.write("Hello, world")
+@app.route("/index.html")
+def index_html():
+    return local_file("index.html")
 
-class IndexHtml(tornado.web.RequestHandler):
-    def get(self):
-        self.write(file("index.html").read())
+@app.route("/index.css")
+def index_css():
+    return local_file("index.css")
 
-class IndexJs(tornado.web.RequestHandler):
-    def get(self):
-        self.write(file("index.js").read())
+@app.route("/index.js")
+def index_js():
+    return local_file("index.js")
 
-class IndexCss(tornado.web.RequestHandler):
-    def get(self):
-        self.write(file("index.css").read())
-
-class Search(tornado.web.RequestHandler):
-    def get(self):
-        return "Placeholder for JSONified records"
-
-
-
-application = tornado.web.Application([
-    (r"/", MainHandler),
-    (r"/index.html", IndexHtml),
-    (r"/index.js", IndexJs),
-    (r"/index.css", IndexCss),
-    (r"/search", Search),
-
-])
-
-if __name__ == "__main__":
-    application.listen(PORT)
-    tornado.ioloop.IOLoop.instance().start()
+if __name__ == '__main__':
+    app.run(debug=True, port=PORT, host="0.0.0.0")
 
